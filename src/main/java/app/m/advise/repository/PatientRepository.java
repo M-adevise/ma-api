48e7a38e0a2efcdf44b7518c36718bf4dbecd110
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2024-06-22 11:14:09 +0300

    chore: filter patients read

--- src/main/java/app/m/advise/repository/PatientRepository.java
+++ src/main/java/app/m/advise/repository/PatientRepository.java
@@ -4,10 +4,17 @@ import app.m.advise.model.Patient;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, String> {
+  @Query(
+      "select p from Patient p where p.lastName ilike %:firstname% or p.lastName ilike %:lastname%")
+  List<Patient> getCriteria(
+      @Param("firstname") String firstName, @Param("lastname") String lastName);
+
   Optional<Patient> findByAuthenticationIdAndEmail(String firebaseId, String email);
 
   List<Patient> findByDoctorId(String dId);
